d1768f7f8cef8b6149e8b6bf8d7cb4f0889b28b8
dackalacbayo <dackalacbayo@gmail.com>
2018-05-30 18:07:16 +0800

    editing banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,13 +9,12 @@ class Banner extends Component {
         <div className="bg-banner opacity position-relative overflow-hidden py-5 text-center">
           <div className="col-md-5 col-lg-8 col-md-8 mx-sm-4 my-5">
             <div className="panel bg">
-            <h2 className="display-4 text-uppercase font-400 text-right text-white">
-              DYNAMICALLY DESIGNING
+            <h2 className="display-4 text-uppercase text-right text-white">
+              Dynamically Designing
             </h2>
-            <h2 className="text-uppercase text-right title-2 font-weight-normal text-right text-white">
-              AND MANUFACTURING QUALITY FURNITURE
+            <h2 className="text-right title-2 font-weight-normal text-right text-white">
+              & manufacturing quality furniture
             </h2>
-            <div className="vertical-line-right-white" />
           </div>
           <div className="panel-button pt-3">
             <Link to="#aboutUs">
@@ -26,6 +25,7 @@ class Banner extends Component {
             <br />
             </div>
           </div>
+
         </div>
       </div>
     );
