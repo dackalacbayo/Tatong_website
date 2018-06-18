eba8584c688b98074edd8753dbc45f54911c924e
dackalacbayo <dackalacbayo@gmail.com>
2018-06-18 15:23:39 +0800

    adding of faq field in netlify cms / install gatsby-transformer-remark and gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,14 @@ module.exports = {
     title: 'Tatong',
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-netlify-cms',
   ],
