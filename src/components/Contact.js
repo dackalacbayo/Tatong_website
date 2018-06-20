fe93d2088012e3b5e50cca1bd2c0387c76cb0138
dackalacbayo <dackalacbayo@gmail.com>
2018-06-20 17:17:57 +0800

    updated / netlify form in contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -34,30 +34,6 @@ class Contact extends Component {
                       </p>
                     </div>
                     <ContactForm />
-
-
-
-                <form name="contactDetails" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
-                  <input type="hidden" name="form-name" value="contactDetails" />
-                  <p>
-                   <label>Your Name: <input type="text" name="name" /></label>
-                  </p>
-                  <p>
-                   <label>Your Email: <input type="email" name="email" /></label>
-                  </p>
-                  <p>
-                   <label>Your Role: <select name="role[]" multiple>
-                     <option value="leader">Leader</option>
-                     <option value="follower">Follower</option>
-                   </select></label>
-                  </p>
-                  <p>
-                   <label>Message: <textarea name="message"></textarea></label>
-                  </p>
-                  <p>
-                   <button type="submit">Send</button>
-                  </p>
-                </form>
                   </div>
                 </div>
               </div>
