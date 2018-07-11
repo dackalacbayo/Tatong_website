f663f14d392315179c25b48b906de1c5e2cdac09
dackalacbayo <33882737+dackalacbayo@users.noreply.github.com>
2018-07-11 11:26:51 +0800

    Update Build.js

--- src/pages/projects/Build.js
+++ src/pages/projects/Build.js
@@ -7,6 +7,7 @@ import image1 from "../../assets/images/image1.jpg";
 import image2 from "../../assets/images/image2.jpg";
 import image3 from "../../assets/images/image3.jpg";
 import image4 from "../../assets/images/image4.jpg";
+import Header from '../../components/Header'
 
 const buildItems = [
   {
@@ -88,6 +89,7 @@ class Build extends Component {
   render() {
     return (
       <div className="pt-5">
+      <Header />
         <PageHeader
           title={"Build Design"}
           breadcrumbItem1={"Home /"}
